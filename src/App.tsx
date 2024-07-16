74a0bc815c64bef21abcdedef80a01314a9bf9ac
Melvin Vermeer <melvin.vermeer@conclusion.nl>
2024-07-16 12:12:44 +0200

    use a formAction

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import { FormEvent, useState } from "react";
+import { useState } from "react";
 import "./App.css";
-
-type TodoForm = HTMLFormElement & { todoItem: HTMLInputElement };
 
 function App() {
   const [todos, setTodos] = useState<string[]>(["Buy milk", "Buy eggs"]);
 
-  const onSubmit = (event: FormEvent<TodoForm>) => {
-    event.preventDefault();
-    setTodos([...todos, event.currentTarget.todoItem.value]);
+  const addTodoAction = (formData: FormData) => {
+    const newTodo = formData.get("todoItem");
+    setTodos([...todos, newTodo as string]);
   };
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input id="todoItem" type="text" />
+      <form action={addTodoAction}>
+        <input name="todoItem" type="text" />
         <button type="submit">Add</button>
       </form>
 
       <ul>
         {todos.map((todo) => (
-          <li>{todo}</li>
+          <li key={todo}>{todo}</li>
         ))}
       </ul>
     </div>
